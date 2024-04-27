af21747df4d47d9feb26ee5fef8468f54e7814dc
Sudipta Kumar Das <sudiptadas2000k@gmail.com>
2024-04-27 23:46:11 +0530

    main:Added cookie parser

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+import { app } from './app.js';
 
+dotenv.config({
+  path: './.env',
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.port || 8000, () => {
+      console.log(`Server is running at port: ${process.env.port}`);
+    });
+  })
+  .catch((err) => {
+    console.log('MongoDB connect fail');
+  });
